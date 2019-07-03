a5887874e8def9f102858d408dead0287336e1de
Aleksey Belchenkov <u608110@gmail.com>
2019-07-03 06:37:15 +0300

    Managing file uploads

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ Route::post('/categories/upsert', 'CategoryController@upsert');
 Route::delete('/categories/{category}', 'CategoryController@destroy');
 
 Route::post('/menu-items/add', 'MenuItemController@store');
+
+Route::post('/add-image', function (Request $request) {
+    $file = $request->file('file');
+    $dir = 'public/images';
+    $path = $file->store($dir);
+
+    return str_replace("$dir/", "", $path);
+});
