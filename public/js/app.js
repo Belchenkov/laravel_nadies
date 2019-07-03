ef7943be5bca7eba1870bf164d953615855cb077
Aleksey Belchenkov <u608110@gmail.com>
2019-07-03 06:02:40 +0300

    Displaying model validation errors

--- public/js/app.js
+++ public/js/app.js
@@ -1890,7 +1890,16 @@ __webpack_require__.r(__webpack_exports__);
     };
   },
   methods: {
-    save: function save() {}
+    save: function save() {
+      var _this = this;
+
+      axios.post('/api/menu-items/add', this.item).then(function (res) {
+        _this.$router.push('/');
+      })["catch"](function (error) {
+        var messages = Object.values(error.response.data.errors);
+        _this.errors = [].concat.apply([], messages);
+      });
+    }
   }
 });
 
@@ -38068,6 +38077,7 @@ var render = function() {
     "form",
     {
       staticClass: "item-form",
+      attrs: { novalidate: "" },
       on: {
         submit: function($event) {
           $event.preventDefault()
